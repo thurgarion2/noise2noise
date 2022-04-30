da93f127bd40a1d3d2b4a49732d9daa2899d91ee
erwan serandour <erwan.serandour@epfl.ch>
2022-04-30 12:52:26 +0200

    implement unet and training loop

--- noise2noise/_nbdev.py
+++ noise2noise/_nbdev.py
@@ -2,13 +2,23 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"load_train": "00_helpers.ipynb",
+index = {"load_images": "00_helpers.ipynb",
          "to_float_image": "00_helpers.ipynb",
          "to_rgb_image": "00_helpers.ipynb",
          "show_img": "00_helpers.ipynb",
-         "psnr": "00_helpers.ipynb"}
+         "psnr": "00_helpers.ipynb",
+         "ToFloatImage": "00_training.ipynb",
+         "Noise2NoiseDataset": "00_training.ipynb",
+         "train": "00_training.ipynb",
+         "eval_model": "00_training.ipynb",
+         "plot_losses": "00_training.ipynb",
+         "UnetBlockDown": "00_unet.ipynb",
+         "UnetBlockUp": "00_unet.ipynb",
+         "Unet": "00_unet.ipynb"}
 
-modules = ["helpers.py"]
+modules = ["helpers.py",
+           "training.py",
+           "unet.py"]
 
 doc_url = "https://thurgarion2.github.io/noise2noise/"
 
