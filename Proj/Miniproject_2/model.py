8de5256c03946ae8dbda522c4c9b9c0952e2a685
JulianBlackwell <julian.blackwell99@gmail.com>
2022-05-24 01:32:51 +0200

    Correct Transpose arguments

--- Proj/Miniproject_2/model.py
+++ Proj/Miniproject_2/model.py
@@ -132,7 +132,7 @@ class Linear(Module):
         '''Return weight and bias parameters'''
         return [(self.W, self.dW), (self.b, self.db)]
 
-class Conv2d(Module): # TODO
+class Conv2d(Module):
     '''Conv2d module implemented by a linear function'''
     def __init__(self, in_channels=3, out_channels=3, kernel_size=(2, 2), stride=1):
         '''Conv2d module constructor
@@ -144,6 +144,8 @@ class Conv2d(Module): # TODO
         :kernel_size: (tuple) Size of the convolving kernel, default = (2, 2)
 
         :stride: (int) Stride of the convolution, default = 1
+
+        :padding: (int) controls the amount of padding applied to the input
         '''
         self.in_channels = in_channels
         self.out_channels = out_channels
@@ -200,26 +202,40 @@ class Conv2d(Module): # TODO
         return [(self.W, self.dW), (self.b, self.db)]
 
 class TransposeConv2d(Module): # TODO
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1):
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.kernel_size = kernel_size
         self.stride = stride
+        self.padding=0
+        self.dilation=1
 
         # Initialize weights & bias
-        sqrt_k = 1 / (in_channels * kernel_size[0] * kernel_size[1]) ** 0.5
+        sqrt_k = 1 / (out_channels * kernel_size[0] * kernel_size[1]) ** 0.5
 
-        self.W = empty((out_channels, in_channels, kernel_size[0], kernel_size[1])).uniform_(-sqrt_k, sqrt_k)
+        self.W = empty((in_channels, out_channels, kernel_size[0], kernel_size[1])).uniform_(-sqrt_k, sqrt_k)
         self.b = empty((out_channels)).uniform_(-sqrt_k, sqrt_k)
 
         # Initialize weights & bias gradients
         self.dW = self.W.new_zeros(self.W.size())
         self.db = self.b.new_zeros(self.b.size())
 
-    def forward(self):
+    def forward(self, input_):
+        '''TransposeConv2d forward pass
+        
+        :input_: (tensor) input tensor
+
+        :returns: 2D transposed convolution operator applied over input
+        '''
         pass
 
-    def backward(self):
+    def backward(self, d_out):
+        '''TransposeConv2d backward pass
+        
+        :d_out: (tensor) Gradient w.r.t. output (backpropagated gradient from next layers)
+
+        :returns: Loss gradient to propagate
+        '''
         pass
 
     def param(self):
@@ -227,7 +243,10 @@ class TransposeConv2d(Module): # TODO
         return [(self.W, self.dW), (self.b, self.db)]
 
 class Upsampling(Module): # TODO (underlying is transposeConv2d)
-    pass
+    '''Upsampling module'''
+    def __init__(self):
+        '''Upsampling constructor'''
+        pass
 
 class MSE(Module):
     '''Mean squared error loss'''
