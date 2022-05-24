2ac6c9aee2ba655c6343319d1a1e06d5d72c30b1
Ghali C <ghalicb@gmail.com>
2022-05-24 11:19:22 +0200

    Add weight decay & momentum for SGD

--- Proj/Miniproject_2/model.py
+++ Proj/Miniproject_2/model.py
@@ -319,7 +319,7 @@ class Sequential(Module):
 
 class SGD:
     '''Stochastic gradient descent optimizer'''
-    def __init__(self, params, learning_rate=0.05):
+    def __init__(self, params, learning_rate=0.05, weight_decay=0, momentum=0, dampening=0):
         '''SGD optimizer constructor
         
         :params: (iterable) - iterable of parameters to optimize
@@ -328,6 +328,10 @@ class SGD:
         '''
         self.params = params
         self.lr = learning_rate
+        self.weight_decay = weight_decay
+        self.momentum = momentum
+        self.dampening = dampening
+        self.velocity = list()
 
     def zero_grad(self):
         '''Sets the gradients of all optimized torch.Tensor to 0'''
@@ -336,6 +340,25 @@ class SGD:
 
     def step(self):
         '''Performs single optimization step'''
+
+        # Add weight decay
+        if (self.weight_decay != 0):
+            for p, g in self.params:
+                g = g + self.weight_decay * p
+        
+        # Add momentum
+        if (self.momentum != 0):
+            if (~self.velocity):
+                for _, g in self.params:
+                    self.velocity.append(g) 
+            else:
+                for i, (p, g) in enumerate(self.params):
+                    self.velocity[i] = self.momentum * self.velocity[i] + (1 - self.dampening) * g
+            
+            for i, (p, g) in enumerate(self.params):
+                g = self.velocity[i]
+
+        # Update parameters
         for p, g in self.params:
             p -= self.lr * g
 
